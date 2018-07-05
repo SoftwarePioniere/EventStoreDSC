aadb1e823cc904ce9c6598fe1328b67db998c776
Tobias Böker <tb@softwarepioniere.de>
2018-07-05 12:25:59 +0200

    ..

--- EventStoreDSC/Examples/Sample1.ps1
+++ EventStoreDSC/Examples/Sample1.ps1
@@ -9,7 +9,6 @@ Configuration Sample1
         {
             RootDrive = "c:"
             ExtIp = "127.0.0.1"
-<<<<<<< HEAD
             CertificatePassword = "Password"
             ProjectName = "test"
             IntHttpPort = "2812"
@@ -18,17 +17,6 @@ Configuration Sample1
             ExtTcpPort = "1813"
             IntSecureTcpPort = "3812"
             ExtSecureTcpPort = "3813"
-=======
-            #CertificatePassword = "xxx"
-            CertificatePassword = "Password"
-            ProjectName = "test"
-            # IntHttpPort = "2912"
-            # ExtHttpPort = "2913"
-            # IntTcpPort = "1912"
-            # ExtTcpPort = "1913"
-            # IntSecureTcpPort = "3912"
-            # ExtSecureTcpPort = "3913"
->>>>>>> e9421b2e35ca352cf8f3fc5a2177e059e3ad8ab5
             OldAdminPassword = "changeit"
             OldOpsPassword = "changeit"
             NewAdminPassword = "changedit"

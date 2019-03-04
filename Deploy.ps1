25fb65016b937b08c73bf0d8a9d3b082f0196939
Tobias Böker <tb@softwarepioniere.de>
2019-03-04 11:42:04 +0100

    added cakebuild

--- Deploy.ps1
+++ Deploy.ps1
@@ -1,3 +1,9 @@
+[CmdletBinding()]
+Param(
+    [string]$version = "9.9.9",
+    [string]$apiKey
+)
+
 #script to deploy with appveyor
 $moduleDir = "$(Get-Location)\xxModules"
 
@@ -13,8 +19,17 @@ if (!(Test-Path -Path "$moduleDir\FileDownloadDSC\1.0.10")) {
     Save-Module -Name FileDownloadDSC -RequiredVersion '1.0.10' -Path $moduleDir
 }
 
+if (!(Test-Path -Path "$moduleDir\cChoco\2.4.0.0")) {
+    Save-Module -Name cChoco -RequiredVersion '2.4.0.0' -Path $moduleDir
+}
+
 $env:PSModulePath = "$env:PSModulePath;$moduleDir";
 Write-Host  $env:PSModulePath
 
-Update-ModuleManifest -Path .\EventStoreDSC\EventStoreDSC.psd1 -ModuleVersion $env:APPVEYOR_BUILD_VERSION
-Publish-Module -Path .\EventStoreDSC -NuGetApiKey $env:PS_GALLERY_API_KEY
+Write-Host "Updating Module Manifest... with Version: $version"
+Update-ModuleManifest -Path .\EventStoreDSC\EventStoreDSC.psd1 -ModuleVersion $version
+
+if ($apiKey) {
+    Write-Host "Publishing Module... With ApiKey: $apiKey"
+    # Publish-Module -Path .\EventStoreDSC -NuGetApiKey $apiKey
+}
---
+++ build.cake
@@ -0,0 +1,28 @@
+#tool nuget:?package=GitVersion.CommandLine&version=4.0.0
+#addin nuget:?package=Cake.Powershell&version=0.4.7
+
+///////////////////////////////////////////////////////////////////////////////
+// ARGUMENTS
+///////////////////////////////////////////////////////////////////////////////
+var target          = Argument("target", "Default");
+var psGalleryApiKey   = Argument("PS_GALLERY_API_KEY", "");
+
+Task("Default")
+  .Does(() =>
+{
+  var version = GitVersion();
+  Information($"Version: {version.NuGetVersionV2}");
+
+  StartPowershellFile("./Deploy.ps1", args =>
+  {
+        args.Append("version", version.AssemblySemFileVer);
+
+        if (!string.IsNullOrEmpty(psGalleryApiKey)) {
+            args.Append("psGalleryApiKey", psGalleryApiKey)
+            ;
+        }
+  });
+
+});
+
+RunTarget(target);
